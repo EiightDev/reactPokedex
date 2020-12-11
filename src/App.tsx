27ec4e649f64c37097b0cfc0f2ab42b8c0f41947
Mustapha <eiightdev@gmail.com>
2020-12-11 10:05:10 +0100

    fichiers initiaux

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from "react";
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import PageNotFound from "./pages/page-not-found";
+import PokemonDetail from "./pages/pokemon-detail";
+import PokemonEdit from "./pages/pokemon-edit";
+import PokemonList from "./pages/pokemon-list";
+
+const App: FunctionComponent = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokédex
+            </Link>
+          </div>
+        </nav>
+      </div>
+      <Switch>
+        <Route exact path="/" component={PokemonList} />
+        <Route exact path="/pokemons" component={PokemonList} />
+        <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
+        <Route path="/pokemons/:id" component={PokemonDetail} />
+        <Route component={PageNotFound}/>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
